ed8bd5818e2f7f3db42fdf1323779397cd413daa
salahudinalayubi <fourtherilak@gmail.com>
2018-10-24 20:14:40 +0700

    tambah file app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+function Mahasiswa(Nama, Nim, Fakultas, Prodi) {
+    this.Nama = Nama;
+    this.Nim = Nim;
+    this.Fakultas = Fakultas;
+    this.Prodi = Prodi;
+}
+
+var NewMahasiswa = new Mahasiswa('Salahudin', '151410336', 'Ilmu Komputer', 'Sistem Informasi');
